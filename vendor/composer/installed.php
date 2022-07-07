263cb4e4507d84554b72ff52a4adeff2572b2116
jellyjes <your@email.address>
2022-07-07 12:52:15 +0800

    initial commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'vendor_name/heroku',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'f6dc573243457f0be0194f61a0545642a4b6e0f3',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'vendor_name/heroku' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'f6dc573243457f0be0194f61a0545642a4b6e0f3',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
