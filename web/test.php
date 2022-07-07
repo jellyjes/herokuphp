c1b7718d6b462958b3c5f195a9117ee0ab4bf25b
jellyjes <your@email.address>
2022-07-07 15:55:18 +0800

    updates

--- web/test.php
+++ web/test.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <!-- fix : does not have meta attribute -->
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Description of the webpage">
     <title>Test</title>
     <!-- fix : Does not have a [title] attribute -->
